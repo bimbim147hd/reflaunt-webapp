bf60225e82b249be693ed9c8c1c116e9c169aca5
lebaotrung <trung1995bao@gmail.com>
2019-07-16 10:17:38 +0700

    Pusher Intergration notification (Test)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,7 @@ export const environment = {
   production: true,
   jwtTokenKey: 'JWT_TOKEN',
   apiUrl: 'https://api.reflaunt.com',
-  authId: 'AUTH_ID'
+  authId: 'AUTH_ID',
+  PUSHER_APP_KEY: '2bf28e4da15d19435318',
+  PUSHER_APP_CLUSTER: 'ap1'
 };
